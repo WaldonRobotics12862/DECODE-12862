cba41faf81595b51776f7dbda5e8b36aab44c0a4
LordDuck69696 <alix.coupteau@gmail.com>
2025-09-20 12:53:16 -0400

    Setup Framework For Intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DigActions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DigActions.java
@@ -1,54 +1,61 @@
 package org.firstinspires.ftc.teamcode;
 
 public class DigActions {
-    public static class Launcher{
+    public static class Launcher {
 
     }
-    public static class Sensors{
+    public static class Sensors {
 
     }
-    public static class Intake{
-        public Intake (HardwareMap hardwareMap){
+    public static class Intake {
+        public Intake(HardwareMap hardwareMap) {
             // 1 Motor, 2 Direction
         }
         public static class IntakeOn implements Actions {
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 return false;
-
             }
         }
-            public static Action intakeOn(){
-                return new IntakeOn();
-            }
+        public static Action intakeOn() {
+            return new IntakeOn();
         }
         public static class IntakeOff implements Actions {
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 return false;
-
             }
         }
-            public static Action intakeOff(){
-                return new IntakeOff();
-            }
+        public static Action intakeOff() {
+            return new IntakeOff();
         }
         public static class SpitOut implements Actions {
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 return false;
-
-            }
-
-            public static Action spitOut() {
-                return new SpitOut();
             }
         }
+        public static Action spitOut() {
+            return new SpitOut();
+        }
     }
-    public static class Hopper{
+    public static class Hopper {
 
     }
-    public static class Parking{
-
+    public static class Parking {
+        public static class ParkServo {
+            public ParkServo(HardwareMap hardwareMap) {
+                // Servo initialization, e.g., configuring servo motor
+            }
+            public static class ServoPark implements Actions {
+                @Override
+                public boolean run(@NonNull TelemetryPacket packet) {
+                    return false;
+                }
+            }
+            public static Action servoPark() {
+                return new ServoPark();
+            }
+        }
     }
 }
