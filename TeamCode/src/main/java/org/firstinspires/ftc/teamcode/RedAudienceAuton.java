450e0edb09365a7ad18cb0b642b4c732ceef6080
LordDuck69696 <alix.coupteau@gmail.com>
2025-10-01 20:15:24 -0400

    Alix added cool auton path code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAudienceAuton.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
+@Autonomous(name="Red Audience Auton")
+@Config
+public class RedAudienceAuton extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        waitForStart();
+        Actions.runBlocking(
+                new SequentialAction(
+
+                )
+        );
+    }
+}
